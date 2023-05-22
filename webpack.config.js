c896e363fcec58dbb8e06ab67bde2a92954883a3
azamat <ahavov_azamat@mail.ru>
2023-05-22 15:29:13 +0300

    Fix3

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,47 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 
-export default {
-  mode: process.env.NODE_ENV || "development",
+// const isProduction = process.env.NODE_ENV == "production";
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
   module: {
     rules: [
-      { test: /\.css$/, use: ["style-loader", "css-loader", "postcss-loader"] },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader", "postcss-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
       },
       {
         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: "url-loader?limit=10000",
+        use: 'url-loader?limit=10000',
       },
       {
         test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: "file-loader",
+        use: 'file-loader',
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "index.html",
+      template: './index.html',
+      inject: 'body',
     }),
   ],
 };
