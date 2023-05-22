0efda48d6bc20b0a3256a2ca473e740c20900a04
azamat <ahavov_azamat@mail.ru>
2023-05-22 16:18:39 +0300

    Fix4

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
-# JavaScript Проект - «RSS агрегатор»
+# RSS Agregator
 ## Описание
 RSS – специализированный формат, предназначенный для описания лент новостей, анонсов статей и других материалов. Это наиболее простой способ для сайтов (обычно, блогов) дать возможность пользователям подписываться на изменения. Для этого используются специальные сервисы, называемые RSS-агрегаторами. Эти сервисы умеют опрашивать RSS-ленты сайтов на наличие новых постов и показывают их в удобном виде, отмечая прочитанное и так далее.
 Rss Reader – сервис для агрегации RSS-потоков, с помощью которых удобно читать разнообразные источники, например, блоги. Он позволяет добавлять неограниченное количество RSS-лент, сам их обновляет и добавляет новые записи в общий поток.
-## Установка
-```sh
-make install
+### Системные требования
+
 ```
-## Запуск локального сервера
-```sh
-make develop
+  node.js
+```
+
+### Установка и запуск
+
+```
+  git clone git@github.com:elisad5791/frontend-project-lvl3.git
+  cd frontend-project-lvl3
+  make install
+  make start
 ```
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/AzamatAk/frontend-project-11/workflows/hexlet-check/badge.svg)](https://github.com/AzamatAk/frontend-project-11/actions) 
 [![Maintainability](https://api.codeclimate.com/v1/badges/caa0527e2f87443e64fd/maintainability)](https://codeclimate.com/github/AzamatAk/frontend-project-11/maintainability)
+
+### Приложение
+
+[RSS Agregator]()
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,22 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+import HTMLWebpackPlugin from 'html-webpack-plugin';
 
-// const isProduction = process.env.NODE_ENV == "production";
-
-module.exports = {
-  mode: process.env.NODE_ENV || 'development',
+export default {
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: 'file-loader',
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      inject: 'body',
-    }),
-  ],
 };
